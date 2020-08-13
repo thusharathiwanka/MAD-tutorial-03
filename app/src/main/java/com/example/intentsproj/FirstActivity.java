762ba8173b5b457af3b30b1d0607f3e0721aee0f
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-13 22:43:08 +0530

    value passing done

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,6 @@ public class FirstActivity extends AppCompatActivity {
     Button button;
     EditText editText1;
     EditText editText2;
-    TextView textView;
     Toast toast;
 
     @Override
@@ -31,16 +31,26 @@ public class FirstActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//              String value1 = editText1.getText().toString();
+//              String value2 = editText2.getText().toString();
+//              int numberValue1 = Integer.parseInt(value1);
+//              int numberValue2 = Integer.parseInt(value2);
+
+                int value1 = Integer.parseInt(editText1.getText().toString());
+                int value2 = Integer.parseInt(editText2.getText().toString());
+
                 Context context = getApplicationContext();
                 CharSequence message = "You just clicked the OK button";
                 int duration = Toast.LENGTH_SHORT;
 
                 toast = Toast.makeText(context, message, duration);
 
-                toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+                toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.BOTTOM, 0, 0);
                 toast.show();
 
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("VALUE1", value1);
+                intent.putExtra("VALUE2", value2);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -2,13 +2,31 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText1;
+    EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.result);
+
+        Intent intent = getIntent();
+        int value1 = intent.getIntExtra("VALUE1", 0);
+        int value2 = intent.getIntExtra("VALUE2", 0);
+
+        editText1 = findViewById(R.id.number1);
+        editText2 = findViewById(R.id.number2);
+
+        editText1.setText(String.valueOf(value1));
+        editText2.setText(String.valueOf(value2));
     }
 }
