afbd1b162adf604d20dba420a30a43a486bb2e77
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-13 21:29:14 +0530

    second layout and toast created

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,17 +2,22 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
     Button button;
     EditText editText1;
     EditText editText2;
     TextView textView;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,21 @@ public class FirstActivity extends AppCompatActivity {
         button = findViewById(R.id.okBtn);
         editText1 = findViewById(R.id.number1);
         editText2 = findViewById(R.id.number2);
-        textView = findViewById(R.id.result);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "You just clicked the OK button";
+                int duration = Toast.LENGTH_SHORT;
 
+                toast = Toast.makeText(context, text, duration);
+
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.show();
+
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
